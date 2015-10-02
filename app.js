6f7d1de27f706a296ade117bea8507fba86845ce
Scott Maxwell <scott@scotteh.me>
2015-10-02 23:46:53 +0100

    add client

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ wsServer.on('request', function(request) {
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+            connection.sendUTF(message.utf8Data + ' received!');
         }
         else if (message.type === 'binary') {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
