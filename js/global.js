083e23920559023d48007f8b57b36e1fff30cd6a
Scott Maxwell <scott@scotteh.me>
2015-10-06 16:31:02 +0100

    Continue tidying code

--- js/global.js
+++ js/global.js
@@ -1,16 +1,19 @@
-var Global = {
-    leaderboard: [],
-    users: {},
-    console: false,
-    powerup: [],
-    poweruptype: []
+/*jslint node: true */
+/*jslint plusplus: true */
+"use strict";
+
+var i, Global = {
+  leaderboard: [],
+  users: {},
+  console: false,
+  powerup: [],
+  poweruptype: []
 };
 
 Global.arena = new Array(20);
-for (var i = 0; i < 20; i++) {
+for (i = 0; i < 20; i++) {
   Global.arena[i] = new Array(20);
   Global.arena[i].fill(false);
 }
-
 
 module.exports = Global;
--- js/utils.js
+++ js/utils.js
@@ -1,26 +1,32 @@
-var Global = require('./global.js')
+/*jslint node: true */
+/*jslint bitwise: true */
+"use strict";
+
+var Global = require('./global.js');
 
 exports.randint = function (a, b) {
-    return Math.floor(Math.random() * b) + a; 
-}
+  return Math.floor(Math.random() * b) + a;
+};
 
 exports.uuid = function () {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-}
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
 
 exports.datelog = function (s) {
-    console.log((new Date()) + ' ' + s);
-}
+  console.log((new Date()) + ' ' + s);
+};
 
 exports.sendConsole = function (s) {
-    if (!Global.console) return false;
-    Global.console.sendUTF(s);
-    console.log("sending " + s + " to console");
-}
+  if (!Global.console) {
+    return false;
+  }
+  Global.console.sendUTF(s);
+  console.log("sending " + s + " to console");
+};
 
-exports.arraysEqual = function (a1,a2) {
-    return JSON.stringify(a1)==JSON.stringify(a2);
-}
+exports.arraysEqual = function (a1, a2) {
+  return JSON.stringify(a1) === JSON.stringify(a2);
+};
